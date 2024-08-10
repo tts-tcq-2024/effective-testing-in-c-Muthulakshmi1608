514a15f912b3da4409ed13ac1b89c9d14ec449b5
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-08-10 20:01:21 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -7,16 +7,12 @@ int networkAlertStub(float celcius) {
       // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
-    if(celcius>0)
+    if(celcius==NaN)
     {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    return 200;
+    return 500;
     }
-    else
-    {
-        return 500;
-    }
-    
+   return 200;
 }
 
 void alertInCelcius(float farenheit) {
@@ -36,6 +32,7 @@ int main() {
     alertInCelcius(303.6);
     alertInCelcius(1000.0);
     alertInCelcius(32.0);
+    alertInCelcius(NaN);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
